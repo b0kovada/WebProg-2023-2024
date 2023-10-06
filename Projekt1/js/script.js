ad39dbd86db970d8c288c5a832bf0f5fbd0d20cc
2020b-Kovácsházi Ádám <74898218+b0kovada@users.noreply.github.com>
2023-10-06 11:29:42 +0200

    Projekt 1 - Kész

--- Projekt1/js/script.js
+++ Projekt1/js/script.js
@@ -1,4 +1,6 @@
 function sotetMod(){
     var x = document.getElementById("container");
     x.classList.toggle("sotetModStilus");
+    var y = document.getElementById("h1");
+    y.classList.toggle("sotetModH1");
 }
