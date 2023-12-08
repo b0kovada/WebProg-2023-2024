bcc6b0e5c1e5fc1c0a4abc7388dabf1c6eafec6e
2020b-Kovácsházi Ádám <74898218+b0kovada@users.noreply.github.com>
2023-12-08 09:47:24 +0100

    Event_handler_listener feladat - Kész

---
+++ Event_handler_listener/js/script.js
@@ -0,0 +1,7 @@
+function logSzoveg() {
+    const gomb = document.querySelector(".btn");
+    gomb.addEventListener("click",()=>{
+    let szoveg = document.getElementById("szovegBe").value;
+    console.log("A felhasználó által beírt szöveg: " + szoveg);
+})
+}
