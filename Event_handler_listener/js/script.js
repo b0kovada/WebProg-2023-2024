c3bc3507436013feb633d9a21375977c05ba65fa
2020b-Kovácsházi Ádám <74898218+b0kovada@users.noreply.github.com>
2023-12-08 09:46:43 +0100

    Delete Event_handler_listener directory

--- Event_handler_listener/js/script.js
+++
@@ -1,7 +0,0 @@
-function logSzoveg() {
-    const gomb = document.querySelector(".btn");
-    gomb.addEventListener("click",()=>{
-    let szoveg=document.getElementById("szovegBe").value;
-    console.log("A felhasználó által beírt szöveg :" + szoveg);
-})
-}
