cbd98941188e3fb6d1932b41c4b0635943158ac4
2020b-Kovácsházi Ádám <74898218+b0kovada@users.noreply.github.com>
2024-04-05 12:40:52 +0200

    Kész

--- fetch_methods/fetch_methods.js
+++ fetch_methods/fetch_methods.js
@@ -1,23 +1,14 @@
-fetch("https://jsonplaceholder.typicode.com/comments", {
-     
-    // Metódus hozzáadása
-    method: "POST",
-     
-    // Küldendő test vagy tartalom hozzáadása
-    body: JSON.stringify({
-        title: "foo",
-        body: "bar",
-        userId: 1
-    }),
-     
-    // Fejlécek hozzáadása a kéréshez
+fetch("https://jsonplaceholder.typicode.com/todos/4", {
+    method: "DELETE",
     headers: {
         "Content-type": "application/json; charset=UTF-8"
     }
 })
- 
-// Konvertálás JSON-ba
-.then(response => response.json())
- 
-// Az eredmények megjelenítése a konzolon
-.then(json => console.log(json));
+.then(response => {
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error('Network response was not ok.');
+})
+.then(json => console.log(json))
+.catch(error => console.error('There was a problem with the fetch operation:', error));
