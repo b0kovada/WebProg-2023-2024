bb8cb0f1f56e00b3bacb1564939213688551a2d4
2020b-Kovácsházi Ádám <74898218+b0kovada@users.noreply.github.com>
2024-05-24 09:32:46 +0200

    Feladat - Kész

---
+++ iras_olvasas/zeneszoveg.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+if (!fs.existsSync('./zeneszoveg')) {
+  fs.mkdir('./zeneszoveg', err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Mappa létrehozva!');
+  });
+} else {
+      console.log('A mappa már korábban lérte lett hozva.');
+}
+
+async function iras (){
+    await fs.writeFile('./zeneszoveg/szoveg.txt', '起来！不愿做奴隶的人们！\n把我们的血肉，筑成我们新的长城！\n中华民族到了最危险的时候，\n每个人被迫着发出最后的吼声。\n起来！起来！起来！\n我们万众一心，\n冒着敌人的炮火，前进！\n冒着敌人的炮火，前进！\n前进！前进！进！', () => {
+    console.log('A fájlba beíródott a kínai Himnusz szövege!');
+  });}
+
+  iras();
+
+fs.readFile('./zeneszoveg/szoveg.txt', (err, data) => {
+    if (err) {
+      console.log(err);
+    }  
+    console.log("Kiolvasott adat:" + data.toString());
+  });
